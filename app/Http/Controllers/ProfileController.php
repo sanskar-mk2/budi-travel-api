0a61123e81f38c476b9aa295304984728690175c
Sanskar <sanskar.mk2@gmail.com>
2023-01-18 17:46:33 +0530

    Update profile to include name and make image optional

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -23,21 +23,34 @@ class ProfileController extends Controller
     public function update(Request $request)
     {
         $request->validate([
-            'profile_picture' => 'required|image|mimes:jpg,png,jpeg,gif,svg|max:2048',
+            'profile_picture' => 'nullable|image|mimes:jpg,png,jpeg,gif,svg|max:2048',
             'instagram_handle' => 'nullable|string',
             'tiktok_handle' => 'nullable|string',
             'facebook_handle' => 'nullable|string',
+            'name' => 'nullable|string',
         ]);
 
-        $image_path = $request->file('profile_picture')->store('pfps', 'public');
-        $user = $request->user();
+        if ($request->name) {
+            $user = $request->user();
+            $user->update([
+                'name' => $request->name,
+            ]);
+        }
+
         $profile = $user->profile;
         $profile->update([
-            'profile_picture' => $image_path,
-            'instagram_handle' => $request->instagram_handle,
-            'tiktok_handle' => $request->tiktok_handle,
-            'facebook_handle' => $request->facebook_handle,
+            'instagram_handle' => $request->instagram_handle ?? $profile->instagram_handle,
+            'tiktok_handle' => $request->tiktok_handle ?? $profile->tiktok_handle,
+            'facebook_handle' => $request->facebook_handle ?? $profile->facebook_handle,
         ]);
+
+        if ($request->profile_picture) {
+            $image_path = $request->file('profile_picture')->store('pfps', 'public');
+            $user = $request->user();
+            $profile->update([
+                'profile_picture' => $image_path,
+            ]);
+        }
 
         return response()->json([
             'message' => 'Successfully updated profile',
